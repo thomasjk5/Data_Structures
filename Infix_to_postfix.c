d2bd64f7a1f91f473507a1667bc077a188b37530
Thomas J <43922701+thomasjk5@users.noreply.github.com>
2019-10-17 15:17:13 +0530

    Update Infix_to_postfix.c

--- Infix_to_postfix.c
+++ Infix_to_postfix.c
@@ -1,197 +1,193 @@
 #include<stdio.h>
+#include<stdlib.h>
 #include<string.h>
-#include<math.h>
-int top=-1,top1=-1;
-char s[20],a[20];
-int isop(char c)
-	{
-	  if(c=='A'||c=='B'||c=='C'||c=='D'||c=='E'||c=='F'||c=='G'||c=='a'||c=='b'||c=='c'||c=='d'||c=='e'||c=='f'||c=='g')
-	     return 1;
-	  else
-	     return 0;
-	 }
+char s[50],item;
+int top=-1,top2=-1,n,st[50];
 void push(char item)
-  
-         {
-            top++;
-            s[top]=item;
-         
-      }
-void push1(char item)
-  
-         {
-            top1++;
-           a[top1]=item;
-         
-      }      
+    {
+    top++;
+    s[top]=item;
+    }
+void push2(int it)
+    {
+        top2++;
+        st[top2]=it;
+    }
 char pop()
-     {
-       
-       
-        
-       char item=s[top];
-         top=top-1;
-         return item;
-        
-      } 
-char pop1()
-     {
-       
-       
-        
-       char item=a[top1];
-         top1=top1-1;
-         return item;
-        
-      } 
+{
+  top--;
+  return s[top+1];
+}
+int pop2()
+{
+  top2--;
+  return st[top2+1];
+}
+
 int isp(char c)
-	{
-		
-		     if(c=='+'||c=='-')
-                          {
-                            return 2;
-                            
-                          }
-                     else if(c=='*'||c=='/')
-                          {
-                            return 4;
-                            
-                          }
-                     else if(c=='^')
-                          {
-                           return 5;
-                           
-                          }
-                     else if(c=='('||c==')')
-                          {
-                           return 0;
-                           
-                          }
-                    
-	}
+{
+ switch(c)
+ {
+ case '+':
+ case '-':
+ 	return 2;
+ case '*':
+ case '/':
+ 	return 4;
+ case '^':
+ 	return 5;
+ case '(':
+ case '#':
+ 	return 0;
+ }
+}
 int icp(char c)
-	{
-		
-		     if(c=='+'||c=='-')
-                          {
-                            return 1;
-                            
-                          }
-                     else if(c=='*'||c=='/')
-                          {
-                            return 3;
-                            
-                          }
-                     else if(c=='^')
-                          {
-                           return 6;
-                           
-                          }
-                     else if(c=='(')
-                          {
-                           return 9;
-                           
-                          }
-                     else if(c==')')
-			return 0;
-                    
-	}
-		        
-void ifpf(char e[])
-	{
-	  int i=0;
-	  char token;
-          push('#');
-	  while(e[i]!='#')
-	  	{
-	  	  token=e[i];
-	  	  if(isop(token)==1)
-	  	     printf("%c",token);
-	  	  else if(token==')')
-	  	     {
-	  	       while(s[top]!='(')
-	  	            {
-	  	               printf("%c",pop());
-	  	             }
-	  	             pop();
-	  	      }
-	  	   else
-	  	      {
-	  	        while(isp(s[top])>=icp(token))
-	  	             {
-	  	                printf("%c",pop());
-	  	             }
-                               push(token);
-                       }        	  	                             
-	  	     i=i+1;
-	  	 }
-	  while(s[top]!='#')
-	      {
-	         printf("%c",pop());
-	      }	
-	                 
-	  } 
-int eval(char op1,char op2,char item)
-	{
-		int a,b,c;		
-		printf("Enter value of %c :",op1);
-                scanf("%d",&a);	
-		printf("Enter value of %c :",op2);
-                scanf("%d",&b);
-		if(item=='+')
-			c=a+b;
-		else if(item=='-')
-			c=a-b;
-		else if(item=='*')
-			c=a*b;
-		else if(item=='/')
-			c=a/b;
-		else if(item=='^')
-			c=a*b;
-		return c;
-	}					  
-int postfix(char p[])
-	{
-		char item,op1,op2;
-		int result;
-                int i=0;		
-		while(p[i]!='#')
-			{
-				item=p[i];
-				
-				if(isop(item)==1)
-					push1(item);
-			         
-			        else
-			        	{
-			        	 	op2=pop1();
-			        	 	op1=pop1();
-			        	 	result=eval(op1,op2,item);
-			        	
-			                        push1(result);
-			                 }
-			       i++;
-			   }
-			   result=pop1();
-			   //printf("%c",result); 
-                           return result;      	 
-	}		   	
-				
+{
+ switch(c)
+ {
+ case '+':
+ case '-':
+ 	return 1;
+ case '*':
+ case '/':
+ 	return 3;
+ case '^':
+ 	return 6;
+ case '(':
+ 	return 9;
+ case ')':
+ case '#':
+ 	return 0;
+ }
+}
+int cal(int x,int y,char opr)
+{
+ int res;
+ switch(opr)
+ {
+  case '+':
+  	res=x+y;
+  	break;
+  case '-':
+  	res=x-y;
+  	break;
+  case '*':
+  	res=x*y;
+  	break;
+  case '/':
+  	res=y/x;
+  	break;
+  case '^':
+      res=1;
+      while(x>0)
+      {
+          res=res*y;
+          x--;
+      }
+  	break;
+ }
+ return res;
+}
 
+void eval(char p[])
+{
+ int i=0,op1,op2;
+ while(p[i]!='\0')
+ {
+  if(isdigit(p[i]))
+  {
+    int pp=p[i];
+    push2(pp-48);
+  }
+  else if(isalnum(p[i]))
+    {
+     int v;
+     printf("\nEnter the value of %c:",p[i]);
+     scanf("%d",&v);
+     push2(v);
+    }
+  else
+   {
+    op1=pop2();
+    op2=pop2();
+    int result=cal(op1,op2,p[i]);
+    push2(result);
+   }
+   i++;
+  }
+  printf("\n\tResult=%d\n\n",pop2());
+}
+void intopo(char exp[])
+{
+ strcat(exp,"#");
+ push('#');
+ int i=0,j=0;
+ char post[50];
+ while(exp[i]!='#')
+ {
+  if(isalnum(exp[i])==1)
+  {
+   post[j]=pop();
+   j++;
+  }
+  else if(exp[i]==')')
+  {
+   while(s[top]!='(')
+   {
+    post[j]=pop();
+    j++;
+   }
+   pop();
+  }
+  else
+  {
+   while(isp(s[top])>=icp(exp[i]))
+   {
+    post[j]=pop();
+    j++;
+   }
+   push(exp[i]);
+  }
+  i++;
+ }
+ while(s[top]!='#')
+ {
+  post[j]=pop();
+  j++;
+ }
+ post[j]='\0';
+ printf("Postfix Expression is : %s",post);
+ top=-1;
+ eval(post);
+}
 void main()
-	{
-	 	
-	 	char e[20],p[20];
-	 	printf("Enter the Expression : ");
-	 	scanf("%s",p);
-	        srtcat(p,'#');
-	 	
-
-
-
-
-	 	int r=postfix(p);
-             printf("%d",r);
-	}	 
-	          
-
+{
+ 	char e[50];
+ 	int opt,end=0;
+ 	do
+    {
+ 	printf("#####MENU#####\n1.To convert infix to postfix\n2.To evaluate a post fix expression\n3.exit the program\nEnter a option:");
+ 	scanf("%d",&opt);
+ 	switch(opt)
+ 	{
+    case 1:
+        printf("Enter the infix expression:");
+        scanf("%s",&e);
+        end=1;
+        intopo(e);
+        break;
+ 	case 2:
+         printf("Enter the postfix expression:");
+ 	     scanf("%s",&e);
+ 	     end=1;
+ 	     eval(e);
+         break;
+ 	case 3:
+ 	    end=1;
+        break;
+    default:
+        printf("Invalid option....enter a option given in the menu.\n\n");
+        break;
+    }
+    }while(end!=1);
+}
